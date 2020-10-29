f10cd9d72f8e44d49aba38685bdd62d982c62f3c
Abir D <61226387+Abir-Das@users.noreply.github.com>
2020-10-29 17:03:44 +0530

    video section added

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 require('../scss/app.scss')
+import videojs from 'video.js';
 
 try {
     window._ = require('lodash')
     window.$ = window.jQuery = require('jquery')
     require('bootstrap')
     window.moment = require('moment')
-    window.baseUrl = document
-        .querySelector('meta[name=base-url]')
-        .getAttribute('content')
+    // window.baseUrl = document
+    //     .querySelector('meta[name=base-url]')
+    //     .getAttribute('content')
+
+    // From Frontend
 } catch (e) {
     console.log(e)
 }
@@ -17,3 +20,12 @@ window.axios = require('axios')
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 window.axios.defaults.withCredentials = true
+
+// Frontend
+const homepage_video = videojs(document.querySelector('#homepage-video'), {
+	controls: true,
+	autoplay: false,
+	preload: 'auto',
+	fluid: true
+});
+
